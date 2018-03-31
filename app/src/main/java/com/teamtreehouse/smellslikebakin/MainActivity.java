bb1899173e42c30ee4b53c21227ce9ee65b78714
Yahia Hassan <yehia4747@gmail.com>
2018-04-01 00:06:01 +0200

    Add ViewPager inside the details fragment

--- app/src/main/java/com/teamtreehouse/smellslikebakin/MainActivity.java
+++ app/src/main/java/com/teamtreehouse/smellslikebakin/MainActivity.java
@@ -1,25 +1,42 @@
 package com.teamtreehouse.smellslikebakin;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.OnRecipeSelectedInterface {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String LIST_FRAGMENT = "list_fragment";
+    public static final String VIEWPAGER_FRAGMENT = "viewpager_fragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListFragment fragment = new ListFragment();
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.placeHolder, fragment)
-                .commit();
+        ListFragment savedFragment = (ListFragment) getSupportFragmentManager().findFragmentByTag(LIST_FRAGMENT);
+        if (savedFragment == null) {
+            Log.d(TAG, "onCreate called");
+            ListFragment fragment = new ListFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.placeHolder, fragment, LIST_FRAGMENT)
+                    .commit();
+        }
+
 
     }
 
     @Override
     public void onListRecipeSelected(int index) {
-        Toast.makeText(this, Recipes.names[index], Toast.LENGTH_SHORT).show();
+        ViewPagerFragment fragment = new ViewPagerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt(ViewPagerFragment.KEY_RECIPE_INDEX, index);
+        fragment.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.placeHolder, fragment, VIEWPAGER_FRAGMENT)
+                .addToBackStack(null)
+                .commit();
     }
 }
